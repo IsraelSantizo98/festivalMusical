c0daab923a6a2e4502e11b190caa6da9be72067a
Jose Israel Santizo Santos <joseisraelss.jiss@gmail.com>
2022-02-21 22:01:49 -0600

    Se agrega id de referencia a enlaces de nav y comentarios a codigo js de scrollIntoView

--- src/js/app.js
+++ src/js/app.js
@@ -22,9 +22,10 @@ function navegacionFija() {
 }
 function scrollNav() {
     const enlaces = document.querySelectorAll('.navegacion-principal a');
+    //El evento click al ser mas de un elemento a se debe usar foreach para iterar uno por uno
     enlaces.forEach( enlace => {
         enlace.addEventListener('click', function(e) {
-            e.preventDefault();
+            e.preventDefault();//Se usa pare evitar el comportamiento por defualt
             const seccionScroll = e.target.attributes.href.value;
             const seccion = document.querySelector(seccionScroll);
             seccion.scrollIntoView({ behavior: "smooth"});
