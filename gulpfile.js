fc258bf28a80ba7a67732433171f59e8337c44f7
Jose Israel Santizo Santos <joseisraelss.jiss@gmail.com>
2022-02-21 23:01:35 -0600

    Se crea mapa por optimizacion de codigo

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,25 @@
 const { src, dest, watch, parallel } = require('gulp'); //Retorna multiples funciones de lo contrario se coloca nombre
 const sass = require("gulp-sass")(require('sass')); //Ir a la carpeta de node-module gulp-sass para poder compilarlo
 const plumber = require('gulp-plumber');
+const autoprefixer = require ('autoprefixer');//Se asegura que funcione con el navegador que escojamos
+const cssnano = require ('cssnano'); //Comprime el codigo
+const postcss = require ('gulp-postcss'); //Hace transformaciones por medio de autoprofixer y cssnano
+const sourcemaps = require ('gulp-sourcemaps');
 //Imagenes
 const cache = require('gulp-cache');
 const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 const avif= require('gulp-avif');
+//Javascript
+const terser = require ('gulp-terser-js');
 /* Crear tareas como funcion, callback, fn o done para terminar tarea*/
 function css (done){   
     src('src/scss/**/*.scss')//Identificar el archivo a compilar o colocar **/* para que este pendiente de cambios en cualquier archivo .scss
+        .pipe(sourcemaps.init())
         .pipe (plumber()) //evitar tener el work flow
         .pipe( sass())//Leer el archivo y compilarlo
+        .pipe( postcss([ autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write('.'))
         .pipe( dest('build/css'))//Almacenar el archivo
     done();
 }
@@ -44,7 +53,10 @@ function versionAvif(done){
 }
 function javascript(done){
     src('src/js/**/*.js')
-        .pipe(dest('build/js'));
+    .pipe(sourcemaps.init())
+    .pipe(terser())
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('build/js'));
     done();
 }
 function dev(done){
@@ -54,7 +66,7 @@ function dev(done){
 }
 /* Habilitar tarea para la terminal */
 exports.css = css;
-exports.javascript = javascript;
+exports.js = javascript;
 exports.imagenes = imagenes;
 exports.avif = avif;
 exports.versionWebp = versionWebp;
